d1486fdb72f6c80326f6d120baee592cda0bcf98
siska <code@eth.si>
2013-07-31 08:16:29 +0200

    --sfv now works standalone too (rev. 10)
    Signed-off-by: siska <code@eth.si>

--- dirupdate.c
+++ dirupdate.c
@@ -148,49 +148,50 @@ typedef struct sig_jmp_buf {
 	void *callback, *arg;
 } sigjmp, *p_sigjmp;
 
-static uLong crc_32_tab[] = { /* CRC polynomial 0xedb88320 */0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
-		0xe963a535, 0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
-		0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
-		0xf3b97148, 0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
-		0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec, 0x14015c4f, 0x63066cd9,
-		0xfa0f3d63, 0x8d080df5, 0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
-		0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b, 0x35b5a8fa, 0x42b2986c,
-		0xdbbbc9d6, 0xacbcf940, 0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
-		0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423,
-		0xcfba9599, 0xb8bda50f, 0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
-		0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d, 0x76dc4190, 0x01db7106,
-		0x98d220bc, 0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
-		0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d,
-		0x91646c97, 0xe6635c01, 0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
-		0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457, 0x65b0d9c6, 0x12b7e950,
-		0x8bbeb8ea, 0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
-		0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7,
-		0xa4d1c46d, 0xd3d6f4fb, 0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
-		0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9, 0x5005713c, 0x270241aa,
-		0xbe0b1010, 0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
-		0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81,
-		0xb7bd5c3b, 0xc0ba6cad, 0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
-		0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683, 0xe3630b12, 0x94643b84,
-		0x0d6d6a3e, 0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
-		0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb,
-		0x196c3671, 0x6e6b06e7, 0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
-		0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5, 0xd6d6a3e8, 0xa1d1937e,
-		0x38d8c2c4, 0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
-		0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55,
-		0x316e8eef, 0x4669be79, 0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
-		0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f, 0xc5ba3bbe, 0xb2bd0b28,
-		0x2bb45a92, 0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
-		0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a, 0x9c0906a9, 0xeb0e363f,
-		0x72076785, 0x05005713, 0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
-		0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21, 0x86d3d2d4, 0xf1d4e242,
-		0x68ddb3f8, 0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
-		0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69,
-		0x616bffd3, 0x166ccf45, 0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
-		0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db, 0xaed16a4a, 0xd9d65adc,
-		0x40df0b66, 0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
-		0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693,
-		0x54de5729, 0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
-		0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d };
+static uLong crc_32_tab[] = { /* CRC polynomial 0xedb88320 */0x00000000,
+		0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f, 0xe963a535,
+		0x9e6495a3, 0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988, 0x09b64c2b,
+		0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2, 0xf3b97148,
+		0x84be41de, 0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7, 0x136c9856,
+		0x646ba8c0, 0xfd62f97a, 0x8a65c9ec, 0x14015c4f, 0x63066cd9, 0xfa0f3d63,
+		0x8d080df5, 0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172, 0x3c03e4d1,
+		0x4b04d447, 0xd20d85fd, 0xa50ab56b, 0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6,
+		0xacbcf940, 0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59, 0x26d930ac,
+		0x51de003a, 0xc8d75180, 0xbfd06116, 0x21b4f4b5, 0x56b3c423, 0xcfba9599,
+		0xb8bda50f, 0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924, 0x2f6f7c87,
+		0x58684c11, 0xc1611dab, 0xb6662d3d, 0x76dc4190, 0x01db7106, 0x98d220bc,
+		0xefd5102a, 0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433, 0x7807c9a2,
+		0x0f00f934, 0x9609a88e, 0xe10e9818, 0x7f6a0dbb, 0x086d3d2d, 0x91646c97,
+		0xe6635c01, 0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e, 0x6c0695ed,
+		0x1b01a57b, 0x8208f4c1, 0xf50fc457, 0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea,
+		0xfcb9887c, 0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65, 0x4db26158,
+		0x3ab551ce, 0xa3bc0074, 0xd4bb30e2, 0x4adfa541, 0x3dd895d7, 0xa4d1c46d,
+		0xd3d6f4fb, 0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0, 0x44042d73,
+		0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9, 0x5005713c, 0x270241aa, 0xbe0b1010,
+		0xc90c2086, 0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f, 0x5edef90e,
+		0x29d9c998, 0xb0d09822, 0xc7d7a8b4, 0x59b33d17, 0x2eb40d81, 0xb7bd5c3b,
+		0xc0ba6cad, 0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a, 0xead54739,
+		0x9dd277af, 0x04db2615, 0x73dc1683, 0xe3630b12, 0x94643b84, 0x0d6d6a3e,
+		0x7a6a5aa8, 0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1, 0xf00f9344,
+		0x8708a3d2, 0x1e01f268, 0x6906c2fe, 0xf762575d, 0x806567cb, 0x196c3671,
+		0x6e6b06e7, 0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc, 0xf9b9df6f,
+		0x8ebeeff9, 0x17b7be43, 0x60b08ed5, 0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4,
+		0x4fdff252, 0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b, 0xd80d2bda,
+		0xaf0a1b4c, 0x36034af6, 0x41047a60, 0xdf60efc3, 0xa867df55, 0x316e8eef,
+		0x4669be79, 0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236, 0xcc0c7795,
+		0xbb0b4703, 0x220216b9, 0x5505262f, 0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92,
+		0x5cb36a04, 0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d, 0x9b64c2b0,
+		0xec63f226, 0x756aa39c, 0x026d930a, 0x9c0906a9, 0xeb0e363f, 0x72076785,
+		0x05005713, 0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38, 0x92d28e9b,
+		0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21, 0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8,
+		0x1fda836e, 0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777, 0x88085ae6,
+		0xff0f6a70, 0x66063bca, 0x11010b5c, 0x8f659eff, 0xf862ae69, 0x616bffd3,
+		0x166ccf45, 0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2, 0xa7672661,
+		0xd06016f7, 0x4969474d, 0x3e6e77db, 0xaed16a4a, 0xd9d65adc, 0x40df0b66,
+		0x37d83bf0, 0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9, 0xbdbdf21c,
+		0xcabac28a, 0x53b39330, 0x24b4a3a6, 0xbad03605, 0xcdd70693, 0x54de5729,
+		0x23d967bf, 0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94, 0xb40bbe37,
+		0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d };
 
 #define UPDC32(octet,crc) (crc_32_tab[((crc)\
      ^ ((BYTE)octet)) & 0xff] ^ ((crc) >> 8))
@@ -239,6 +240,7 @@ uLong crc32buf(uLong crc32, BYTE *buf, size_t len) {
 #define F_OPT_HAS_G_REGEX	0x2000
 #define F_OPT_VERBOSE4 		0x4000
 #define F_OPT_BUFFER		0x8000
+#define F_OPT_FORCEWSFV		0x10000
 
 #define F_MD_NOREAD			0x1
 
@@ -540,6 +542,7 @@ char *hpd_up =
 				"  --nowbuffer           Disable write pre-caching (faster but less safe), applies to -r\n"
 				"  --memlimit=<bytes>    Maximum file size that can be pre-buffered into memory\n"
 				"  --sfv                 Generate new SFV files inside target folders, works with -r, -u and -s\n"
+				"                        Used by itself, it goes into -r (fs rebuild) mode, but does not change dirlog\n"
 				"                           Avoid using this if when doing a full recursive rebuild\n"
 				"  --exec <..command..{[base]dir}..{user}..{group..{size}..{files}..{created}..>\n"
 				"                         While parsing dirlog/filesystem, execute command for each record\n"
@@ -821,7 +824,6 @@ long get_file_size(char *file);
 time_t get_file_creation_time(struct stat *st);
 int dirlog_write_record(struct dirlog *buffer, off_t offset, int whence);
 ULLONG dirlog_find(char *dirname, int mode, unsigned int flags, void *callback);
-int join_string(char *input[], char *output, int size, int count, char del);
 int enum_dir(char *dir, void *cb, void *arg, int f);
 int file_exists(char *file);
 int update_records(char *dirname, int depth);
@@ -1102,9 +1104,22 @@ int main(int argc, char *argv[]) {
 		gfl |= F_OPT_HAS_G_REGEX;
 	}
 
-	if (gfl & F_OPT_VERBOSE && !(gfl & F_OPT_MODE_RAWDUMP)) {
-		if (gfl & F_OPT_NOBUFFER)
+	if ((gfl & F_OPT_SFV) && updmode != UPD_MODE_REBUILD) {
+		updmode = UPD_MODE_RECURSIVE;
+		if (!(gfl & F_OPT_NOWRITE)) {
+			gfl |= F_OPT_FORCEWSFV|F_OPT_NOWRITE;
+		}
+
+		if ((gfl & F_OPT_VERBOSE) && !(gfl & F_OPT_MODE_RAWDUMP)) {
+			printf(
+					"NOTE: switching to non-destructive filesystem rebuild mode\n");
+		}
+	}
+
+	if ((gfl & F_OPT_VERBOSE) && !(gfl & F_OPT_MODE_RAWDUMP)) {
+		if (gfl & F_OPT_NOBUFFER) {
 			printf("NOTE: disabling memory buffering\n");
+		}
 	}
 
 	actdl.flags |= F_GH_ISDIRLOG;
@@ -1186,7 +1201,7 @@ int dirlog_check_dupe(void) {
 			if (gfl & F_OPT_KILL_GLOBAL) {
 				break;
 			}
-			if (actdl.buffer.pos && actdl.buffer.pos->flags & F_MD_NOREAD) {
+			if (actdl.buffer.pos && (actdl.buffer.pos->flags & F_MD_NOREAD)) {
 				continue;
 			}
 			if (g_bmatch(d_ptr, &actdl)) {
@@ -1431,7 +1446,7 @@ int dirlog_check_records(void) {
 		return 2;
 	}
 
-	if (!actdl.buffer_count && gfl & F_OPT_FIX) {
+	if (!actdl.buffer_count && (gfl & F_OPT_FIX)) {
 		printf(
 				"ERROR: internal buffering must be enabled when fixing, increase limit with --memlimit (see --help)\n");
 	}
@@ -1547,7 +1562,7 @@ int dirlog_check_records(void) {
 				}
 			}
 
-			if (gfl & F_OPT_FORCE && d_ptr->uptime != buffer4.uptime) {
+			if ((gfl & F_OPT_FORCE) && d_ptr->uptime != buffer4.uptime) {
 				printf(
 						"WARNING: [%s] folder creation dates in dirlog and on disk do not match (dirlog:%u, filesystem:%u)\n",
 						d_ptr->dirname, d_ptr->uptime, buffer4.uptime);
@@ -1570,7 +1585,7 @@ int dirlog_check_records(void) {
 
 	}
 
-	if (!(gfl & F_OPT_KILL_GLOBAL) && gfl & F_OPT_FIX && r) {
+	if (!(gfl & F_OPT_KILL_GLOBAL) && (gfl & F_OPT_FIX) && r) {
 		if (rebuild_data_file(DIRLOG, &actdl)) {
 			printf(MSG_GEN_DFRFAIL, DIRLOG);
 		}
@@ -1586,7 +1601,7 @@ int g_bmatch(void *d_ptr, struct g_handle *hdl) {
 	int r_e = g_do_exec(d_ptr, hdl);
 
 	if (exec_str && WEXITSTATUS(r_e)) {
-		if (gfl & F_OPT_VERBOSE && !(gfl & F_OPT_MODE_RAWDUMP)) {
+		if ((gfl & F_OPT_VERBOSE) && !(gfl & F_OPT_MODE_RAWDUMP)) {
 			printf(
 					"WARNING: [%d] external call returned non-zero, ignoring this record\n",
 					WEXITSTATUS(r_e));
@@ -1594,10 +1609,10 @@ int g_bmatch(void *d_ptr, struct g_handle *hdl) {
 		return 1;
 	}
 
-	if (gfl & F_OPT_HAS_G_REGEX && hdl->flags & F_GH_ISDIRLOG
+	if ((gfl & F_OPT_HAS_G_REGEX) && (hdl->flags & F_GH_ISDIRLOG)
 			&& reg_match(GLOB_REGEX, ((struct dirlog*) d_ptr)->dirname,
 					glob_regex_flags) == glob_reg_i_m) {
-		if (gfl & F_OPT_VERBOSE && !(gfl & F_OPT_MODE_RAWDUMP)) {
+		if ((gfl & F_OPT_VERBOSE) && !(gfl & F_OPT_MODE_RAWDUMP)) {
 
 			printf("WARNING: %s: regex match positive, ignoring this record\n",
 					((struct dirlog*) d_ptr)->dirname);
@@ -1641,7 +1656,7 @@ int dirlog_print_stats(void) {
 				}
 			}
 
-			if (gfl & F_OPT_VERBOSE2 && !(gfl & F_OPT_MODE_RAWDUMP)) {
+			if ((gfl & F_OPT_VERBOSE2) && !(gfl & F_OPT_MODE_RAWDUMP)) {
 				struct nukelog n_buffer = { 0 };
 				if (nukelog_find(d_ptr->dirname, 2, &n_buffer) < MAX_ULLONG) {
 					e.nukelog = &n_buffer;
@@ -1950,8 +1965,9 @@ int update_records(char *dirname, int depth) {
 	arg.depth = depth;
 	arg.dirlog = &buffer;
 
-	if (gfl & F_OPT_VERBOSE && gfl & F_OPT_NOWRITE)
+	if ((gfl & F_OPT_VERBOSE) && (gfl & F_OPT_NOWRITE)) {
 		printf("NOTE: performing dry run, no writing will be done\n");
+	}
 
 	return enum_dir(dirname, proc_section, &arg, 0);
 
@@ -1975,7 +1991,7 @@ int proc_release(char *name, unsigned char type, void *arg) {
 		fn2 = strdup(name);
 		fn3 = strdup(name);
 		base = basename(fn3);
-		if (gfl & F_OPT_SFV
+		if ((gfl & F_OPT_SFV)
 				&& (updmode == UPD_MODE_RECURSIVE || updmode == UPD_MODE_SINGLE)
 				&& reg_match(PREG_SFV_SKIP, name,
 				REG_ICASE | REG_NEWLINE) && reg_match(PREG_SFV_SKIP_EXT, fn2,
@@ -1987,8 +2003,12 @@ int proc_release(char *name, unsigned char type, void *arg) {
 			sprintf(buffer, "%s/%s.sfv", dirname(fn2), dn);
 			char buffer2[1024];
 			sprintf(buffer2, "%s %.8X\n", base, (unsigned int) crc32);
-			if (!(gfl & F_OPT_NOWRITE) && !write_file_text(buffer2, buffer))
-				printf("ERROR: failed writing to SFV file: '%s'\n", buffer);
+			if (!(gfl & F_OPT_NOWRITE) || (gfl & F_OPT_FORCEWSFV)) {
+				if (!write_file_text(buffer2, buffer)) {
+					printf("ERROR: %s: failed writing to SFV file: '%s'\n",
+							name, buffer);
+				}
+			}
 			iarg->flags |= F_EARG_SFV;
 			g_strncpy(iarg->buffer, buffer, strlen(buffer));
 			sprintf(buffer, "  %.8X", (unsigned int) crc32);
@@ -2033,7 +2053,7 @@ int proc_section(char *name, unsigned char type, void *arg) {
 	switch (type) {
 	case DT_DIR:
 		iarg->depth--;
-		if (!iarg->depth || gfl & F_OPT_FORCE) {
+		if (!iarg->depth || (gfl & F_OPT_FORCE)) {
 			if (gfl & F_OPT_UPDATE) {
 				if (((rl = dirlog_find(name, 1, F_DL_FOPEN_REWIND, NULL))
 						< MAX_ULLONG)) {
@@ -2066,7 +2086,7 @@ int proc_section(char *name, unsigned char type, void *arg) {
 				goto end;
 			}
 
-			if (gfl & F_OPT_SFV && iarg->flags & F_EARG_SFV) {
+			if ((gfl & F_OPT_SFV) && (iarg->flags & F_EARG_SFV)) {
 				iarg->dirlog->bytes += get_file_size(iarg->buffer);
 				iarg->dirlog->files++;
 				printf("SFV: succesfully generated '%s'\n",
@@ -2088,7 +2108,7 @@ int proc_section(char *name, unsigned char type, void *arg) {
 
 			char buffer[2048] = { 0 };
 
-			if (gfl & F_OPT_VERBOSE
+			if ((gfl & F_OPT_VERBOSE)
 					&& dirlog_format_block(name, iarg, buffer) > 0) {
 				printf(buffer);
 			}
@@ -2131,11 +2151,11 @@ int release_generate_block(char *name, ear *iarg) {
 
 	time_t orig_ctime = get_file_creation_time(&st);
 
-	if (gfl & F_OPT_SFV && !(gfl & F_OPT_NOWRITE)) {
+	if ((gfl & F_OPT_SFV) && !(gfl & F_OPT_NOWRITE)) {
 		enum_dir(name, delete_file, (void*) "\\.sfv$", 0);
 	}
 
-	if (gfl & F_OPT_VERBOSE2 && !(iarg->flags & F_EAR_NOVERB))
+	if ((gfl & F_OPT_VERBOSE2) && !(iarg->flags & F_EAR_NOVERB))
 		printf("ENTERING: %s\n", name);
 
 	if ((r = enum_dir(name, proc_release, iarg, 0)) < 1
@@ -2147,10 +2167,10 @@ int release_generate_block(char *name, ear *iarg) {
 	}
 	g_setjmp(0, "release_generate_block(2)", NULL, NULL);
 
-	if (gfl & F_OPT_VERBOSE2 && !(iarg->flags & F_EAR_NOVERB))
+	if ((gfl & F_OPT_VERBOSE2) && !(iarg->flags & F_EAR_NOVERB))
 		printf("EXITING: %s\n", name);
 
-	if (gfl & F_OPT_SFV && !(gfl & F_OPT_NOWRITE)) {
+	if ((gfl & F_OPT_SFV) && !(gfl & F_OPT_NOWRITE)) {
 		if (gfl & F_OPT_FOLLOW_LINKS) {
 			if (stat(name, &st2)) {
 				ret = 1;
@@ -2454,7 +2474,7 @@ int rebuild_data_file(char *file, struct g_handle *hdl) {
 	sprintf(hdl->s_buffer, "%s.dtm", file);
 	sprintf(buffer, "%s.bk", file);
 
-	if (hdl->buffer_count && (exec_str || gfl & F_OPT_HAS_G_REGEX)
+	if (hdl->buffer_count && (exec_str || (gfl & F_OPT_HAS_G_REGEX))
 			&& updmode != UPD_MODE_RECURSIVE) {
 		g_setjmp(0, "rebuild_data_file(2)", NULL, NULL);
 		if (gfl & F_OPT_VERBOSE2) {
@@ -2558,7 +2578,7 @@ int rebuild_data_file(char *file, struct g_handle *hdl) {
 						errno);
 				ret = 4;
 			}
-//remove(hdl->s_buffer);
+
 		} else {
 			if ((r = rename(hdl->s_buffer, file))) {
 				printf("ERROR: %s: [%d] renaming temporary file failed!\n",
@@ -2604,11 +2624,6 @@ int flush_data_md(struct g_handle *hdl, char *outfile) {
 		}
 	}
 
-	/*if (strlen(hdl->mode)) {
-	 printf("-%s\n", hdl->mode);
-	 mode = hdl->mode;
-	 }*/
-
 	if ((fh = gg_fopen(outfile, mode)) == NULL) {
 		return 2;
 	}
@@ -2635,7 +2650,6 @@ int flush_data_md(struct g_handle *hdl, char *outfile) {
 		}
 		g_memcpy(&buffer[rw * hdl->block_sz], ptr->ptr, hdl->block_sz);
 		rw++;
-		//printf("%d %d %d\n", rw, v, hdl->buffer_count);
 		if (!(gfl & F_OPT_NOWRITE) && (rw == v || !ptr->next)) {
 			if ((bw = g_fwrite(buffer, hdl->block_sz, rw, fh)) != rw) {
 				ret = 3;
@@ -2675,8 +2689,6 @@ int load_data_md(pmda md, char *file, struct g_handle *hdl) {
 		return -2;
 	}
 
-//void *buffer = calloc(1, hdl->block_sz);
-
 	if (!(fh = gg_fopen(file, "r"))) {
 		return -3;
 	}
@@ -2711,10 +2723,7 @@ int load_data_md(pmda md, char *file, struct g_handle *hdl) {
 			goto end;
 		}
 
-		//g_memcpy(ptr, buffer, hdl->block_sz);
 		hdl->buffer_count++;
-
-		//b_read++;
 	}
 
 	g_setjmp(0, "load_data_md", NULL, NULL);
@@ -2722,7 +2731,6 @@ int load_data_md(pmda md, char *file, struct g_handle *hdl) {
 	r = hdl->buffer_count;
 
 	end: g_fclose(fh);
-//r_end: g_free(buffer);
 
 	return r;
 }
@@ -2793,7 +2801,7 @@ int g_fopen(char *file, char *mode, unsigned int flags, struct g_handle *hdl,
 		gh_rewind(hdl);
 	}
 
-	if (!(gfl & F_OPT_NOBUFFER) && flags & F_DL_FOPEN_BUFFER
+	if (!(gfl & F_OPT_NOBUFFER) && (flags & F_DL_FOPEN_BUFFER)
 			&& !(hdl->flags & F_GH_NOMEM)) {
 		if (!g_buffer_into_memory(file, hdl, size)) {
 			if (!(flags & F_DL_FOPEN_FILE)) {
