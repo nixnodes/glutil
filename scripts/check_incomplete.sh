18b40c93e1049ab0825b8341b031b12977d2dc22
nixnodes <code@eth.si>
2013-08-15 04:18:44 +0200

    Checks a release for incomplete/corrupt files by comparing SFV data with filesystem
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/check_incomplete.sh
+++ scripts/check_incomplete.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# DO NOT EDIT BELOW LINE
+# DO NOT EDIT THESE LINES
 #@MACRO:incomplete:{m:exe} -d -exec "'{m:spec1}' '{dir}' '{exe}' '{glroot}'"
 #
 ## Checks a release for incomplete/corrupt files by comparing SFV data with filesystem
 #
 ## Usage (manual): /glroot/bin/dirupdate -d -exec "/glftpd/bin/scripts/check_incomplete.sh '{dir}' '{exe}' '{glroot}'"
 #
-##  Usage (macro): ./dirupdate -m incomplete
+## Usage (macro): ./dirupdate -m incomplete
 #
 ##  To use this macro, place script in the same directory (or any subdirectory) where dirupdate is located
 #
