15c8688652c4c303a14c5dea34258e79a2870c08
nixnodes <code@eth.si>
2013-08-19 11:42:11 +0200

    Retrieves game info using giantbomb API (XML)
    Signed-off-by: nixnodes <code@eth.si>

---
+++ scripts/gb_get.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# DO NOT EDIT THESE LINES
+#@MACRO:getscore:{m:exe} -x {m:arg1} --silent --dir -vvvv --exec "{m:spec1} $(basename {arg}) score"
+#
+## Retrieves game info using giantbomb API (XML)
+#
+CURL="/usr/bin/curl"
+CURL_FLAGS="--silent"
+XMLLINT="/usr/bin/xmllint"
+
+! [ -f "$CURL" ] && CURL=$(whereis curl | awk '{print $2}')
+! [ -f "$XMLLINT" ] && XMLLINT=$(whereis xmllint | awk '{print $2}')
+
+###########################[ BEGIN OPTIONS ]#############################
+
+URL="http://www.giantbomb.com/api"
+API_KEY="e0c8aa999e45d61f9ada46be9d983f24fdd5e288"
+
+INPUT_CLEAN_REGEX="([._-\(\)]{,1}(MULTI|Crack|DOX).*)|(-[A-Z0-9a-z_-]*)$"
+
+############################[ END OPTIONS ]##############################
+
+QUERY=$(echo $1 | sed -r "s/($INPUT_CLEAN_REGEX)//gi" | sed -r "s/[._-\(\)]/+/g" | sed -r "s/^[+ ]+//"| sed -r "s/[+ ]+$//")
+
+WHAT=$2
+
+[ "$WHAT" = "score" ] && FIELD="reviews"
+
+[ -z "$FIELD" ] && exit 1
+[ -z "$QUERY" ] && exit 2
+
+APIKEY_STR="?api_key=$API_KEY"
+
+API_DETAIL_URL=$($CURL $CURL_FLAGS "$URL/search/$APIKEY_STR&query=\"$QUERY\"&limit=1" | $XMLLINT --xpath "string((/response/results/game/api_detail_url)[1])" -)
+
+[ -z "$API_DETAIL_URL" ] && echo "PARSE: '$QUERY': could not find [@$URL/search/?api_key=$API_KEY&query=\"$QUERY\"&limit=1]" && exit 1
+
+API_DETAIL_URL2=$($CURL $CURL_FLAGS "$API_DETAIL_URL$APIKEY_STR&field_list=$FIELD" | $XMLLINT --xpath "string((/response/results/$FIELD//api_detail_url[1]))" -)
+
+[ -z "$API_DETAIL_URL2" ] && echo "PARSE: '$QUERY': could not parse $API_DETAIL_URL$APIKEY_STR&field_list=$FIELD" && exit 1
+
+RES=$($CURL $CURL_FLAGS "$API_DETAIL_URL2$APIKEY_STR&field_list=$WHAT" | $XMLLINT --xpath "string((/response/results/$WHAT)[1])" -)
+
+[ -z "$RES" ] && echo "PARSE: '$QUERY': could not get result '$WHAT' from @$API_DETAIL_URL2" && exit 1
+
+[ "$WHAT" = "score" ] && { 
+ 	[ $RES -gt -1 ] || exit 1
+    [ $RES -lt 11 ] || exit 1
+    echo "SCORE: '$QUERY': $RES"
+}
+
+
