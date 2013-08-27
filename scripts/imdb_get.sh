805b759728f7179cc177b84b1cf851f961d0b946
nixnodes <code@eth.si>
2013-08-27 15:21:25 +0200

    Gets movie info using iMDB native API and omdbapi (XML)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/imdb_get.sh
+++ scripts/imdb_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DO NOT EDIT THESE LINES
-#@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" --regex '{m:arg2}'
-#@MACRO:imdb-c:{m:exe} -x {m:arg1} --cdir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" --regex '{m:arg2}'
+#@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" {m:arg2}"
+#@MACRO:imdb-c:{m:exe} -x {m:arg1} --cdir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" {m:arg2}"
 #@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} {basedir} '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex "{m:arg1}" 
 #
 ## Gets movie info using iMDB native API and omdbapi (XML)
