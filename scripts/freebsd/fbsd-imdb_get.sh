8c8a2bbfd52e38ea77161c41066b2591b7bf53c3
Bsugar9 <bsugar9@gmail.com>
2013-09-04 19:56:51 -0400

    freebsd-imdb_get.sh working script but from older version

--- scripts/freebsd/fbsd-imdb_get.sh
+++ scripts/freebsd/fbsd-imdb_get.sh
@@ -1,9 +1,7 @@
 #!/usr/local/bin/bash
 # DO NOT EDIT THESE LINES
-#@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdb
-file}' '{glroot}' '{siterootn}' '{arg}'"
-#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:
-spec1} {basedir} '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex "{m:arg1}"
+#@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'"
+#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} {basedir} '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex "{m:arg1}"
 #
 ## Gets movie info using iMDB native API and omdbapi (XML)
 #
@@ -12,8 +10,7 @@ spec1} {basedir} '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex
 ## Usage (macro): ./glutil -m imdb --arg1=/path/to/movies
 ##                ./glutil -m imdb-d --arg1 "\/xvid\/[^,]{3,}"
 #
-##  To use this macro, place script in the same directory (or any subdirectory) where glutil is lo
-cated
+##  To use this macro, place script in the same directory (or any subdirectory) where glutil is located
 #
 CURL="/usr/local/bin/curl"
 CURL_FLAGS="--silent"
@@ -36,49 +33,40 @@ IMDBURL="http://www.imdb.com/"
 ## Updates imdblog
 UPDATE_IMDBLOG=1
 #
-INPUT_SKIP="^(.* complete .*|sample|subs|covers|cover|proof|cd[0-9]{1,3}|dvd[0-9]{1,3}|nuked\-.*|.
-* incomplete .*)$"
+INPUT_SKIP="^(.* complete .*|sample|subs|covers|cover|proof|cd[0-9]{1,3}|dvd[0-9]{1,3}|nuked\-.*|.* incomplete .*)$"
 #
-INPUT_CLEAN_REGEX="([\\.\\_\\-\\(\\)](FESTIVAL|(720|1080)[ip]|RERIP|UNRATED|DVDSCR|TC|TS|CAM|EXTEN
-DED|TELESYNC|DVDR|X264|HDTV|SDTV|PDTV|XXX|WORKPRINT|OVA|SUBBED|DUBBED|DOCU|THEATRICAL|RETAIL|SUBFI
-X|NFOFIX|DVDRIP|[1-2][0-9]{3,3}|HDRIP|BRRIP|BDRIP|LIMITED|PROPER|REPACK|XVID)[\\.\\_\\-\\(\\)].*)|
-(-[A-Z0-9a-z_-]*)$"
+INPUT_CLEAN_REGEX="([\\.\\_\\-\\(\\)](FESTIVAL|(720|1080)[ip]|RERIP|UNRATED|DVDSCR|TC|TS|CAM|EXTENDED|TELESYNC|DVDR|X264|HDTV|SDTV|PDTV|XXX|WORKPRINT|OVA|SUBBED|DUBBED|DOCU|THEATRICAL|RETAIL|SUBFIX|NFOFIX|DV
+DRIP|[1-2][0-9]{3,3}|HDRIP|BRRIP|BDRIP|LIMITED|PROPER|REPACK|XVID)[\\.\\_\\-\\(\\)].*)|(-[A-Z0-9a-z_-]*)$"
 #
 ############################[ END OPTIONS ]##############################
 
 #echo "$1" | grep -P -i "$INPUT_SKIP" > /dev/null && exit 1
 echo "$1" | grep -w -i "$INPUT_SKIP" > /dev/null && exit 1
 
-QUERY=$(echo "$1" | tr ' ' '+' | sed -r "s/($INPUT_CLEAN_REGEX)//gI" | sed -r "s/[\\.\\_\\-\\(\\)]
-/+/g" | sed -r "s/^[+ ]+//"| sed -r "s/[+ ]+$//")
+QUERY=$(echo "$1" | tr ' ' '+' | sed -r "s/($INPUT_CLEAN_REGEX)//gI" | sed -r "s/[\\.\\_\\-\\(\\)]/+/g" | sed -r "s/^[+ ]+//"| sed -r "s/[+ ]+$//")
 
 [ -z "$QUERY" ] && exit 1
 
 imdb_search()
 {
-        $CURL $CURL_FLAGS "$IMDBURL""xml/find?xml=1&nr=1&tt=on&q=$1" | xmllint --xpath "((/IMDbRes
-ults//ImdbEntity)[1]/@id)" - 2> /dev/null | sed -r 's/(id\=)|( )|[\"]//g'
+        $CURL $CURL_FLAGS "$IMDBURL""xml/find?xml=1&nr=1&tt=on&q=$1" | xmllint --xpath "((/IMDbResults//ImdbEntity)[1]/@id)" - 2> /dev/null | sed -r 's/(id\=)|( )|[\"]//g'
 }
 
 iid=$(imdb_search "$QUERY""&ex=1")
-[ -z "$iid" ] && echo "WARNING: $QUERY: $1: exact match failed, performing loose search.." && iid=
-$(imdb_search "$QUERY")
-[ -z "$iid" ] && echo "WARNING: $QUERY: $1: $IMDBURL""xml/find?xml=1&nr=1&tt=on&q=$QUERY search fa
-iled, falling back to secondary" && iid=$($CURL $CURL_FLAGS "$URL?r=xml&s=$QUERY" | xmllint --xpat
-h "((/root/Movie)[1]/@imdbID)" - 2> /dev/null | sed -r 's/(imdbID\=)|(\s)|[\"]//g')
+[ -z "$iid" ] && echo "WARNING: $QUERY: $1: exact match failed, performing loose search.." && iid=$(imdb_search "$QUERY")
+[ -z "$iid" ] && echo "WARNING: $QUERY: $1: $IMDBURL""xml/find?xml=1&nr=1&tt=on&q=$QUERY search failed, falling back to secondary" && iid=$($CURL $CURL_FLAGS "$URL?r=xml&s=$QUERY" | xmllint --xpath "((/root/
+Movie)[1]/@imdbID)" - 2> /dev/null | sed -r 's/(imdbID\=)|(\s)|[\"]//g')
 [ -z "$iid" ] && echo "ERROR: $QUERY: $1: cannot find record [$URL?r=xml&s=$QUERY]" && exit 1
 
 DDT=$($CURL $CURL_FLAGS "$URL""?r=XML&i=$iid")
 
 echo "$URL""?r=XML&i=$iid"
 
-[ -z "$DDT" ] && echo "ERROR: $QUERY: $1: unable to get movie data [http://www.omdbapi.com/?r=XML&
-i=$iid]" && exit 1
+[ -z "$DDT" ] && echo "ERROR: $QUERY: $1: unable to get movie data [http://www.omdbapi.com/?r=XML&i=$iid]" && exit 1
 
 get_field()
 {
-        echo $DDT | xmllint --xpath "((/root/movie)[1]/@$1)" - 2> /dev/null | sed -r "s/($1\=)|(^[
- ]+)|([ ]+$)|[\"]//g"
+        echo $DDT | xmllint --xpath "((/root/movie)[1]/@$1)" - 2> /dev/null | sed -r "s/($1\=)|(^[ ]+)|([ ]+$)|[\"]//g"
 }
 
 RATING=$(get_field imdbRating)
@@ -89,29 +77,22 @@ TITLE=$(get_field title)
 RATED=$(get_field rated)
 ACTORS=$(get_field actors)
 DIRECTOR=$(get_field director)
-#RELEASED=`date --date="$(D_g=$(get_field released); [ "$D_g" != "N/A" ] && echo "$D_g" || echo ""
-)" +"%s"`
+RELEASED=`date -j -f '%d %b %Y' "$(D_g=$(get_field released); [ "$D_g" != "N/A" ] && echo "$D_g" || echo "")" +"%s"`
 RUNTIME=$(get_field runtime)
 RUNTIME_h=$(echo $RUNTIME | awk '{print $1}' | sed -r 's/[^0-9]+//g')
 RUNTIME_m=$(echo $RUNTIME | awk '{print $3}' | sed -r 's/[^0-9]+//g')
 [ -z "$RUNTIME_m" ] && RUNTIME=$RUNTIME_h || RUNTIME=$[RUNTIME_h*RUNTIME_m]
 
-[ -z "$RATING" ] && [ -z "$VOTES" ] && [ -z "$GENRE" ] && echo "ERROR: $QUERY: $1: could not extra
-ct movie data" && exit 1
+[ -z "$RATING" ] && [ -z "$VOTES" ] && [ -z "$GENRE" ] && echo "ERROR: $QUERY: $1: could not extract movie data" && exit 1
 
 if [ $UPDATE_IMDBLOG -eq 1 ]; then
         trap "rm /tmp/glutil.img.$$.tmp; exit 2" SIGINT SIGTERM SIGKILL SIGABRT
         GLR_E=$(echo $4 | sed 's/\//\\\//g')
         DIR_E=$(echo $6 | sed "s/^$GLR_E//" | sed "s/^$GLSR_E//")
         $2 -a --iregex "$DIR_E" --imatchq > /dev/null || $2 -e imdb --match "$DIR_E" > /dev/null
-        #echo -en "dir $DIR_E\ntime $(date +%s)\nimdbid $iid\nscore $RATING\ngenre $GENRE\nvotes $
-VOTES\ntitle $TITLE\nactors $ACTORS\nrated $RATED\nyear $YEAR\nreleased $RELEASED\nruntime $RUNTIM
-E\ndirector $DIRECTOR\n\n" > /tmp/glutil.img.$$.tmp
-        echo -en "dir $DIR_E\ntime $(date +%s)\nimdbid $iid\nscore $RATING\ngenre $GENRE\nvotes $V
-OTES\ntitle $TITLE\nactors $ACTORS\nrated $R
-ATED\nyear $YEAR\nreleased nruntime $RUNTIME\ndirector $DIRECTOR\n\n" > /tmp/glutil.img.$$.tmp
-        $2 -z imdb --nobackup --silent < /tmp/glutil.img.$$.tmp || echo "ERROR: failed writing to
-imdblog!!"
+        echo -en "dir $DIR_E\ntime $(date +%s)\nimdbid $iid\nscore $RATING\ngenre $GENRE\nvotes $VOTES\ntitle $TITLE\nactors $ACTORS\nrated $RATED\nyear $YEAR\nreleased $RELEASED\nruntime $RUNTIME\ndirector
+$DIRECTOR\n\n" > /tmp/glutil.img.$$.tmp
+        $2 -z imdb --nobackup --silent < /tmp/glutil.img.$$.tmp || echo "ERROR: failed writing to imdblog!!"
         rm /tmp/glutil.img.$$.tmp
 fi
 
