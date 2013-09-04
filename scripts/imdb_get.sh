0363c3fec77b7d4d5240381014a752deeae57059
nixnodes <code@eth.si>
2013-09-04 05:29:48 +0200

    Gets movie info using iMDB native API and omdbapi (XML)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/imdb_get.sh
+++ scripts/imdb_get.sh
@@ -2,7 +2,7 @@
 # DO NOT EDIT THESE LINES
 #@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" {m:arg2}"
 #@MACRO:imdb-c:{m:exe} -x {m:arg1} --cdir --exec "{m:spec1} $(basename {arg}) '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" {m:arg2}"
-#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} {basedir} '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex "{m:arg1}" 
+#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} '{basedir}' '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregex "{m:arg1}" 
 #
 ## Gets movie info using iMDB native API and omdbapi (XML)
 #
