6cabfc9932d8e3c2e6773f9b69a48e7b63ed09b9
nixnodes <code@eth.si>
2013-09-26 22:47:45 +0200

    Checks a release for incomplete/corrupt files by comparing SFV data with filesystem
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/check_incomplete.sh
+++ scripts/check_incomplete.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # DO NOT EDIT THESE LINES
-#@MACRO:incomplete:{m:exe} -d -exec "'{m:spec1}' '{dir}' '{exe}' '{glroot}' {m:arg1}"
+#@MACRO:incomplete:{m:exe} -d -execv "{m:spec1} {dir} {exe} {glroot} {m:arg1}" --silent
+#@MACRO:incomplete-c:{m:exe} -d -execv "{m:spec1} {dir} {exe} {glroot} {m:arg2}" --iregex "{m:arg1}" --silent
 #
 ## Checks a release for incomplete/corrupt files by comparing SFV data with filesystem
 #
 ## Usage (manual): /glroot/bin/glutil -d -exec "/glftpd/bin/scripts/check_incomplete.sh '{dir}' '{exe}' '{glroot}'"
 #
-## Usage (macro): ./glutil -m incomplete
+## Usage (macro): ./glutil -m incomplete                             (full dirlog)
+##                ./glutil -m incomplete-c -arg1="Unforgettable"     (regex filtered dirlog scan)
 #
 ##  To use this macro, place script in the same directory (or any subdirectory) where glutil is located
 #
@@ -18,7 +20,7 @@
 VERBOSE=0
 #
 ## Optional corruption checking (CRC32 calc & match against .sfv)
-CHECK_CORRUPT=0
+CHECK_CORRUPT=1
 #
 ############################[ END OPTIONS ]##############################
 
@@ -28,27 +30,51 @@ CHECK_CORRUPT=0
 
 GLROOT=$3
 
-DIR=$GLROOT$1
 EXE=$2
 
+
+c_dir() {
+	while read l; do
+		FFL=$(echo "$l" | sed -r 's/ [A-Fa-f0-9]*([ ]*|$)$//')
+#		FCRC=$(echo "$l" | rev | cut -d " " -f -1 | rev)
+#		echo $FFL $FCRC
+		FFT=$(dirname "$1")/$FFL
+		! [ -f "$FFT" ] && echo "WARNING: $DIR: incomplete, missing file: $FFL" && continue
+		[ $CHECK_CORRUPT -gt 0 ] && { 
+			FCRC=$(echo "$l" | rev | cut -d " " -f -1 | rev) && CRC32=$($EXE --crc32 $FFT) && 
+				[ $CRC32 != $FCRC ] && echo "WARNING: $DIR: corrupted: $FFL, CRC32: $CRC32, should be: $FCRC" && continue
+		}
+		[ $VERBOSE -gt 0 ] && echo "OK: $FFL: $FCRC"
+	done < "$1"
+}
+
+
+[ "$4" = "cdir" ] && {
+	DIR="$1"
+
+	[ -n "$5" ] && VERBOSE=1
+	c_dir "$DIR"
+	exit 1
+}
+
+DIR=$GLROOT$1
 [ -n "$4" ] && VERBOSE=1
 
 ! [ -d "$DIR" ] && exit 1
 
+#echo "$0 {arg} $2 $3 cdir"
+
+$EXE -x "$DIR" --iregexi "\.sfv$" -execv "$0 {arg} $2 $3 cdir $4" --silent
+
+exit 1
+
 proc_dir() {
 	for i in $1/*; do
-		if [ -f "$i" ] && echo $i | grep -P "\.sfv$" > /dev/null; then
-			while read l; do
-				FFL=$(echo $l | sed 's/ [A-Fa-f0-9]*.*$//')
-				FCRC=$(echo $l | grep -o -P "[A-Fa-f0-9]*$")
-				FFT=$(dirname $i)/$FFL
-				! [ -f "$FFT" ] && echo "WARNING: $DIR: incomplete, missing file: $FFL" && continue
-				[ $CHECK_CORRUPT -gt 0 ] && CRC32=$($EXE --crc32 $FFT) && [ $CRC32 != $FCRC ] && echo "WARNING: $DIR: corrupted: $FFL, CRC32: $CRC32, should be: $FCRC" && continue
-				[ $VERBOSE -gt 0 ] && echo "OK: $FFL: $FCRC"
-			done < "$i"
-		else
+		if [ -f "$i" ] && echo $i | egrep -q "\.sfv$"; then
+			c_dir "$i"
+		elif [ -d "$i" ]; then
 			pdbn=$(basename "$i")
-			[ -d "$i" ] && [[ "$pdbn" != ".." ]] && [[ "$pdbn" != "." ]] && proc_dir $i
+			[[ "$pdbn" != ".." ]] && [[ "$pdbn" != "." ]] && proc_dir $i
 		fi
 	done
 }
