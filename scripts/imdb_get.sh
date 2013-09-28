c57f3ac0c2750183aa1e68076a9829aa0ee98b77
nixnodes <code@eth.si>
2013-09-28 14:18:58 +0200

    Updated <imdb-d> macro to filter by explicit field (rev. 2)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/imdb_get.sh
+++ scripts/imdb_get.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:1
-#@REVISION:1
+#@REVISION:2
 #@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec `{m:spec1} "`basename '{arg}'`" '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'` {m:arg2}
 #@MACRO:imdb-c:{m:exe} -x {m:arg1} --cdir --exec "{m:spec1} `basename {arg}` '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'" {m:arg2}
-#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} '{basedir}' '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregexi "{m:arg1}" 
+#@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -exec "{m:spec1} '{basedir}' '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{dir}'" --iregexi "dir,{m:arg1}" 
 #@MACRO:imdb-e:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:spec1} '{m:arg1}' '{exe}' '{imdbfile}' '{glroot}' '{siterootn}'"
 #
 ## Gets movie info using iMDB native API and omdbapi (XML)
