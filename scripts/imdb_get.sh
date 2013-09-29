5bab71b2807586fe05e21c364572a9e0e7fbf308
nixnodes <code@eth.si>
2013-09-29 21:02:13 +0200

    <imdb-su> macro (self-update) (rev. 7)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/imdb_get.sh
+++ scripts/imdb_get.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:1
-#@REVISION:6
+#@REVISION:7
 #@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --exec `{m:spec1} "$(basename '{arg}')" '{exe}' '{imdbfile}' '{glroot}' '{siterootn}' '{arg}'` {m:arg2}
 #@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -execv "{m:spec1} {basedir} {exe} {imdbfile} {glroot} {siterootn} {dir}" --iregexi "dir,{m:arg1}" 
+#@MACRO:imdb-su:{m:exe} -a --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -execv "{m:spec1} {dir} {exe} {imdbfile} {glroot} {siterootn} {dir} 1" 
 #@MACRO:imdb-e:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:spec1} '{m:arg1}' '{exe}' '{imdbfile}' '{glroot}' '{siterootn}'"
 #
 ## Gets movie info using iMDB native API and omdbapi (XML)
