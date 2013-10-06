d5870a6bf5be732a39d54bf739dab15bfa04caeb
nixnodes <code@eth.si>
2013-10-06 19:52:20 +0200

    Updated requirements (full list)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/tvrage_get.sh
+++ scripts/tvrage_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:1
-#@REVISION:8
+#@REVISION:9
 #@MACRO:tvrage:{m:exe} -x {m:arg1} --silent --dir -execv `{m:spec1} {basepath} {exe} {tvragefile} {glroot} {siterootn} {arg}` {m:arg2}
 #@MACRO:tvrage-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup tvrage" -execv `{m:spec1} {basedir} {exe} {tvragefile} {glroot} {siterootn} {dir}` --iregexi "dir,{m:arg1}"  {m:arg2} 
 #@MACRO:tvrage-su:{m:exe} -h --silent -v --loglevel=5 --preexec "{m:exe} -v --backup tvrage" -execv `{m:spec1} {basedir} {exe} {tvragefile} {glroot} {siterootn} {dir} 1`
@@ -9,8 +9,9 @@
 #
 ## Gets show info using TVRAGE API (XML)
 #
-## Requires: -glutil-1.9-34 or greater
-##			 -libxml2 v2.7.7 or above 
+## Requires: - glutil-1.9-34 or greater
+##			 - libxml2 v2.7.7 or above
+##           - curl, date, egrep, sed
 #
 ## Usage (macro): ./glutil -m tvrage --arg1=/path/to/shows [--arg2=<path filter>]                 (filesystem based)
 ##                ./glutil -m tvrage-d --arg1 '\/tv\-((sd|hd|)x264|xvid)\/.*\-[a-zA-Z0-9\-_]+$'   (dirlog based)
