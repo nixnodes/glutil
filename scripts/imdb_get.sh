1c6a3cf56f7123d1fa4b49dae16be6cdf976b720
nixnodes <code@eth.si>
2013-10-26 00:29:51 +0200

    Undo bad edit (again), no warning on bad/missing mandatory arguments (rev. 9)
    Signed-off-by: nixnodes <code@eth.si>

--- scripts/imdb_get.sh
+++ scripts/imdb_get.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:2
-#@REVISION:8
+#@REVISION:9
 #@MACRO:imdb:{m:exe} -x {m:arg1} --silent --dir --execv `{m:spec1} {basepath} {exe} {imdbfile} {glroot} {siterootn} {path} 0` {m:arg2}
 #@MACRO:imdb-d:{m:exe} -d --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -execv "{m:spec1} {basedir} {exe} {imdbfile} {glroot} {siterootn} {dir} 0" --iregexi "dir,{m:arg1}" 
 #@MACRO:imdb-su:{m:exe} -a --silent -v --loglevel=5 --preexec "{m:exe} -v --backup imdb" -execv "{m:spec1} {dir} {exe} {imdbfile} {glroot} {siterootn} {dir} 1 {year}" 
@@ -67,7 +67,7 @@ RECORD_MAX_AGE=14
 TYPE_SPECIFIC_DB=0
 #
 ## Verbose output
-VERBOSE=0
+VERBOSE=1
 #
 ## Allowed types regular expression (per omdbapi)
 OMDB_ALLOWED_TYPES="movie|N\/A"
@@ -129,7 +129,7 @@ get_field()
 }
 
 
-if ! [[ $7 -eq 2 ]]; then
+if ! [ $7 -eq 2 ]; then
         echo "$TD" | egrep -q -i "$INPUT_SKIP" && exit 1
 
         QUERY=`echo "$TD" | tr ' ' '.' | sed -r "s/$INPUT_CLEAN_REGEX//gi" | sed -r "s/[\.\_\-\(\)]/+/g" | sed -r "s/(^[+ ]+)|([+ ]+\$)//g"`
@@ -141,7 +141,7 @@ if ! [[ $7 -eq 2 ]]; then
         }
 
         [ $IMDB_SEARCH_BY_YEAR -eq 1 ] && {
-                if [[ $7 -eq 1 ]] && [ $IMDB_DATABASE_TYPE -eq 1 ]; then
+                if [ $7 -eq 1 ] && [ $IMDB_DATABASE_TYPE -eq 1 ]; then
                         DENY_IMDBID_DUPE=0
                         YEAR_q="$8"
                 else
