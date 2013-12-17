033f8ec19f3039e659b0f0689e8538273c9f6f4b
nixnodes <code@eth.si>
2013-12-17 03:21:29 +0100

    Add loc_cl.sh and loc_dm.sh
    Signed-off-by: nixnodes <code@eth.si>

---
+++ scripts/loc_cl.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# DO NOT EDIT/REMOVE THESE LINES
+#@VERSION:1
+#@REVISION:0
+#
+#  Copyright (C) 2013 NixNodes
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+#
+## 
+#
+###########################[ BEGIN OPTIONS ]#############################
+#
+## glutil executable
+GLUTIL="/glftpd/bin/glutil"
+#
+## glFTPd root path
+GLROOT="/glftpd"
+#
+#TIMEOUT=60
+#
+###########################[  END OPTIONS  ]#############################
+
+g_io="${GLROOT}/io/glud"
+
+#echo "${@}" | egrep -q '[\$\`]' && exit 3 
+
+! [ -p "${g_io}/gld.in.$$" ] && mkfifo "${g_io}/gld.in.$$"
+
+[ -p "${g_io}/gld.in.$$" ] && echo "${@}" > "${g_io}/gld.in.$$"
+
+ec=1
+
+! [ -p "${g_io}/gld.out.$$" ] && { 
+	mkfifo "${g_io}/gld.out.$$"
+	trap "rm -f ${g_io}/gld.out.$$; exit 2" 2 15 9 6
+	#${GLUTIL} noop --silent --sleep ${TIMEOUT} --fork "kill -9 $$"
+
+	while read line; do
+		echo "${line}"
+		echo "${line}" | egrep -q "^OK$" && ec=0 && break;
+		echo "${line}" | egrep -q "^ERROR$" && ec=2	&& break;
+	done < "${g_io}/gld.out.$$"	 
+}
+
+[ -p "${g_io}/gld.out.$$" ] && rm -f "${g_io}/gld.out.$$"
+
+exit ${ec}
---
+++ scripts/loc_dm.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+#  Copyright (C) 2013 NixNodes
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# DO NOT EDIT/REMOVE THESE LINES
+#@VERSION:1
+#@REVISION:0
+#@MACRO:loc-dm:{m:exe} --glroot "{m:glroot}" -x "{m:glroot}/io/glud" iregex "basepath,gld\.in\." --preexec `mkdir -p {m:glroot}/io/glud` --loop 0 --usleep 300000 --silent -execv "{m:spec1} {path} {?rd:basepath:^gld\.in\.} {m:exe} {glroot}"
+#
+## Offers functionality to processes that would otherwise require higher priviledges.
+## The tasks it does are constricted and injection checks performed against user-
+## specified input.
+## Communicates with other processes via 'loc_cl.sh' using named pipes (FIFO)
+#
+## glutil -x searches for FIFO's in /io/glud in the background, this script
+## is executed only when something is found.
+#
+## Avoid running this as root, use a normal user and give it access to what
+## resources are needed.
+#
+#########################################################################
+
+g_f="${1}"
+g_pid=${2}
+GLUTIL="${3}"
+glroot=${4}
+
+trap "rm -f ${g_f}; exit 2" 2 15 9 6
+
+async_reply() {
+	ar_bp="${glroot}/io/glud/gld.out.${g_pid}"
+	${GLUTIL} noop --silent --preexec "{exe} noop --daemon --preexec \"sleep 5; kill -9 {procid}; rm -f $ar_bp\"" --fork "mkfifo \"${ar_bp}\"; echo \"${1}\" > \"${ar_bp}\"; rm -f \"${ar_bp}\""
+	return 0
+}
+
+process_query(){	
+	case ${1} in
+    imdb-lookup ) # <query> <imdb log path> 
+        ${GLUTIL} -m imdb-e -arg1 "${2}" --imdblog "${3}" &&
+        	${GLUTIL} -q imdb --imdblog "${3}" -vvvv --shmem --shmdestroy --loadq &&
+        	async_reply OK || async_reply BAD
+        ;;
+    tvrage-lookup ) # <query> <tvrage log path> 
+        ${GLUTIL} -m tvrage-e -arg1 "${2}" --i "${3}" &&
+        	${GLUTIL} -q tvrage --tvlog "${3}" -vvvv --shmem --shmdestroy --loadq &&
+        	async_reply OK || async_reply BAD
+        ;;
+    *)
+    	async_reply UNKNOWN
+    	;;
+    esac
+}
+
+e_m=`cat ${1}`
+
+echo "${e_m}" | egrep -q '[\$\`]' && async_reply "ERROR" && {
+	rm -f "${g_f}"
+	exit 3
+}
+
+eval process_query ${e_m}
+
+rm -f "${g_f}"
