3fc7e01a9e6d8a61d31c93dfe29d346ff5b14aa6
nixnodes <code@eth.si>
2014-07-23 21:49:41 +0200

    filelog scripts
    Signed-off-by: nixnodes <code@eth.si>
    Former-commit-id: 753a4a5a428f26a3545c275ca9f4c8a0eae7b9d5

---
+++ scripts/flog/mk_index.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+GLUTIL=/bin/glutil-chroot
+FILE_LOG_PATH=/ftp-data/glutil/db/filelog
+#
+###################################
+#
+
+g_file=`echo "${1}" | cut -f 2- -d " "`
+g_user=${2}
+g_path=`pwd`/${g_file}
+#cur_path=`${GLUTIL} -w imatch status,"STOR ${g_file}" and iregex "user,${g_user}" -print "{dir}"`
+
+${GLUTIL} --silent -x "${g_path}" -print "dir {path}{:n}size {size}{:n}time {mtime}{:n}user {user}{:n}group {group}{:n}status 0{:n}files 0{:n}" --noglconf > /tmp/mki.$$.tmp
+${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} --silent --imatch "dir,${g_path}" --nobuffer && {
+	${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} -vvvv --gz 2 --raw --match "dir,${g_path}" --nobuffer > /tmp/mki.d.$$.tmp
+	mv /tmp/mki.d.$$.tmp ${FILE_LOG_PATH}
+}
+${GLUTIL} -z altlog --nobackup --noglconf --altlog ${FILE_LOG_PATH} --gz 2 --silent < /tmp/mki.$$.tmp
+
+cat /tmp/mki.$$.tmp > /tmp/mki.1
+
+rm /tmp/mki.$$.tmp
+
+exit 0
+
