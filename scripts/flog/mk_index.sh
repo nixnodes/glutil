d9acc5380e0197b36d85f802b089f6a5aea41700
nixnodes <code@eth.si>
2014-07-25 05:24:29 +0200

    optimize for performance, made existing record removal optional
    Signed-off-by: nixnodes <code@eth.si>
    Former-commit-id: 5a8969957fc32cdd5d4b1bbd64f1a095e71b4d84

--- scripts/flog/mk_index.sh
+++ scripts/flog/mk_index.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+#@VERSION:0
+#@REVISION:2
 #
 GLUTIL=/bin/glutil-chroot
+#
 FILE_LOG_PATH=/ftp-data/glutil/db/filelog
+#
+REMOVE_EXISTING=0
 #
 ###################################
 #
@@ -10,16 +15,16 @@ g_file=`echo "${1}" | cut -f 2- -d " "`
 g_user=${2}
 g_path=`pwd`/${g_file}
 
-${GLUTIL} --silent -x "${g_path}" -print "file {path}{:n}size {size}{:n}time {mtime}{:n}user {uid}{:n}group {gid}{:n}status 0{:n}files 0{:n}" --noglconf > /tmp/mki.$$.tmp
-${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} --silent --imatch "file,${g_path}" --nobuffer && {
-	${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} -vvvv --gz 2 --raw --match "file,${g_path}" --nobuffer > /tmp/mki.d.$$.tmp
-	mv /tmp/mki.d.$$.tmp ${FILE_LOG_PATH}
+[ ${REMOVE_EXISTING} -gt 0 ] && {
+	${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} --silent --imatch "file,${g_path}" --nobuffer && {
+		${GLUTIL} -q altlog --altlog ${FILE_LOG_PATH} -vvvv --gz 2 --raw --match "file,${g_path}" --nobuffer > /tmp/mki.d.$$.tmp
+		mv /tmp/mki.d.$$.tmp ${FILE_LOG_PATH}
+	}
 }
-${GLUTIL} -z altlog --nobackup --noglconf --altlog ${FILE_LOG_PATH} --gz 2 --silent < /tmp/mki.$$.tmp
 
-cat /tmp/mki.$$.tmp > /tmp/mki.1
-
-rm /tmp/mki.$$.tmp
+${GLUTIL} --silent -x "${g_path}" \
+ 	-print "file {path}{:n}size {size}{:n}time {mtime}{:n}user {uid}{:n}group {gid}{:n}status 0{:n}files 0{:n}" \
+ 	--noglconf | ${GLUTIL} -z altlog --nobackup --noglconf --altlog ${FILE_LOG_PATH} --gz 2 --silent
 
 exit 0
 
