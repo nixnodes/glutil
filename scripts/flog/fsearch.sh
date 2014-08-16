ac17077ca719f6dc173f904f03a7dae08f72eb24
nixnodes <code@eth.si>
2014-08-16 06:07:26 +0200

    searches made non-buffered
    Signed-off-by: nixnodes <code@eth.si>
    Former-commit-id: 50ca46e9f4a8a40ec522c0ac9a09850f67df2146

--- scripts/flog/fsearch.sh
+++ scripts/flog/fsearch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#@VERSION:0
+#@REVISION:2
 #
 GLUTIL="/bin/glutil-chroot"
 FILE_LOG_PATH=/ftp-data/glutil/db/filelog
@@ -14,7 +16,7 @@ vstr=`echo "${@}" | sed -r 's/ /.*/'`
 	vstr=".*"
 }
 
-${GLUTIL} -q altlog --sort num,asc,time --sortmethod qsort --altlog ${FILE_LOG_PATH} --silent \
+${GLUTIL} -q altlog --nobuffer --altlog ${FILE_LOG_PATH} --silent \
 	iregexi basedir,"${vstr}" -print "{file}  [{?m:size/1024} k] [{?tl:time#%m/%d/%y}][{?tl:time#%H}:{?tl:time#%M}:{?tl:time#%S}]"
 
 exit 0
