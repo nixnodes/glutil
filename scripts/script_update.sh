074538eaba9b33ecdb282f6dcda56082fabb8be3
nixnodes <code@nixnodes.net>
2014-11-29 05:16:27 +0100

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/script_update.sh
+++ scripts/script_update.sh
@@ -17,7 +17,7 @@
 #
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:0
-#@REVISION:13
+#@REVISION:14
 #@MACRO:script-update-self|Update script-update.sh:{exe} -noop --preexec `B="https://raw.githubusercontent.com/nixnodes/glutil/master/scripts/";D="{?d:(spec1)}";[ -d "$\{D\}" ] || mkdir "$\{D\}"; if curl --silent "$\{B\}"script_update.sh > "$\{D\}/script_update.sh"; then echo -e "$\{D\}/script_update.sh \t\tv$(cat "$\{D\}/script_update.sh" | egrep "^\#\@VERSION\:" | cut -d ":" -f 2).$(cat "$\{D\}/script_update.sh" | egrep "^\#\@REVISION\:" | cut -d ":" -f 2) \tOK"; chmod 755 "$\{D\}/script_update.sh"; else echo "$\{D\}/script_update.sh \tFAILED"; fi; `
 #@MACRO:script-update|Install/update native scripts:{exe} -noop --preexec `{spec1} "{arg1}" {glroot}`
 #
@@ -203,6 +203,7 @@ script_process_source()
 					echo "ERROR: ${in_source}: failed loading source"
 					return 1
 				}
+				[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${path}: processing sources"
 				script_process_source INPUT_SOURCES[@] "${match}"
 			}
 		}
@@ -244,5 +245,8 @@ for in_source in "${BASE_PATH}/script_update.d"/*; do
 	script_inject_sources INPUT_SOURCES[@]
 done
 
+[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${path}: processing main sources"
+
 script_process_source SOURCES[@] "${match}"
 
+exit 0
