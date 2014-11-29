539babda8f4f0be6c768593e3262717cdc39fcf3
nixnodes <code@nixnodes.net>
2014-11-29 10:34:50 +0100

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/script_update.sh
+++ scripts/script_update.sh
@@ -17,7 +17,7 @@
 #
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:0
-#@REVISION:20
+#@REVISION:21
 #@MACRO:script-update-self|Update script-update.sh:{exe} -noop --preexec `B="https://raw.githubusercontent.com/nixnodes/glutil/master/scripts/";D="{?d:(spec1)}";[ -d "$\{D\}" ] || mkdir "$\{D\}"; if curl --silent "$\{B\}"script_update.sh > "$\{D\}/script_update.sh"; then echo -e "$\{D\}/script_update.sh \t\tv$(cat "$\{D\}/script_update.sh" | egrep "^\#\@VERSION\:" | cut -d ":" -f 2).$(cat "$\{D\}/script_update.sh" | egrep "^\#\@REVISION\:" | cut -d ":" -f 2) \tOK"; chmod 755 "$\{D\}/script_update.sh"; else echo "$\{D\}/script_update.sh \tFAILED"; fi; `
 #@MACRO:script-update|Install/update native scripts:{exe} -noop --preexec `{spec1} {glroot} "{arg1}" "{arg2}"`
 #
@@ -129,7 +129,7 @@ script_process_source()
 		elif [[ ${opt} -eq 4 ]]; then
 			[ -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" ] && 
 				rm -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" &&
-				echo "WARNING: ${name}: ${path}: deleted ${GLROOT}/${BASE_SEARCHDIR}/${path}"
+				echo "DELETE: ${name}: ${path}"
 			continue
 		elif [[ ${opt} -eq 1 ]]; then
 			[ -f "${GLROOT}${BASE_SEARCHDIR}/${path}" ] && {
