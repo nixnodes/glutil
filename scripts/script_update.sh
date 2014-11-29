ee9319556cfc6f78ba46d2d44520ddc52fff4501
nixnodes <code@nixnodes.net>
2014-11-29 05:06:27 +0100

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/script_update.sh
+++ scripts/script_update.sh
@@ -17,7 +17,7 @@
 #
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:0
-#@REVISION:11
+#@REVISION:12
 #@MACRO:script-update-self|Update script-update.sh:{exe} -noop --preexec `B="https://raw.githubusercontent.com/nixnodes/glutil/master/scripts/";D="{?d:(spec1)}";[ -d "$\{D\}" ] || mkdir "$\{D\}"; if curl --silent "$\{B\}"script_update.sh > "$\{D\}/script_update.sh"; then echo -e "$\{D\}/script_update.sh \t\tv$(cat "$\{D\}/script_update.sh" | egrep "^\#\@VERSION\:" | cut -d ":" -f 2).$(cat "$\{D\}/script_update.sh" | egrep "^\#\@REVISION\:" | cut -d ":" -f 2) \tOK"; chmod 755 "$\{D\}/script_update.sh"; else echo "$\{D\}/script_update.sh \tFAILED"; fi; `
 #@MACRO:script-update|Install/update native scripts:{exe} -noop --preexec `{spec1} "{arg1}" {glroot}`
 #
@@ -112,13 +112,17 @@ script_process_source()
 		}
 		
 		opt=`echo "${item}" | cut -d' ' -f4`
-			
-		[[ ${opt} -eq 1 ]] && { 
+		
+		if [[ ${opt} -eq 2 ]]; then
+			[ -d "${GLROOT}/${BASE_SEARCHDIR}" ] &&
+				touch "${GLROOT}/${BASE_SEARCHDIR}/${path}"
+			[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${path}: updates disabled"
+		elif [[ ${opt} -eq 1 ]]; then
 			[ -f "${GLROOT}${BASE_SEARCHDIR}/${path}" ] && {
 				[ ${VERBOSE} -gt 0 ] &&  echo "WARNING: ${path}: configuration file already exists"
 				continue
 			}
-		}
+		fi
 		
 		echo "${name}" | egrep -q "^${2}$" && {
 			${CURL} ${CURL_FLAGS} "${BASE_URL}/${path}" > /tmp/glutil.script_update.$$.tmp || {
