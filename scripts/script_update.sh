0bcb8b062cc924eb503735b4d531b4205309e87f
nixnodes <code@nixnodes.net>
2014-11-29 10:00:52 +0100

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/script_update.sh
+++ scripts/script_update.sh
@@ -17,9 +17,9 @@
 #
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:0
-#@REVISION:18
+#@REVISION:19
 #@MACRO:script-update-self|Update script-update.sh:{exe} -noop --preexec `B="https://raw.githubusercontent.com/nixnodes/glutil/master/scripts/";D="{?d:(spec1)}";[ -d "$\{D\}" ] || mkdir "$\{D\}"; if curl --silent "$\{B\}"script_update.sh > "$\{D\}/script_update.sh"; then echo -e "$\{D\}/script_update.sh \t\tv$(cat "$\{D\}/script_update.sh" | egrep "^\#\@VERSION\:" | cut -d ":" -f 2).$(cat "$\{D\}/script_update.sh" | egrep "^\#\@REVISION\:" | cut -d ":" -f 2) \tOK"; chmod 755 "$\{D\}/script_update.sh"; else echo "$\{D\}/script_update.sh \tFAILED"; fi; `
-#@MACRO:script-update|Install/update native scripts:{exe} -noop --preexec `{spec1} "{arg1}" {glroot}`
+#@MACRO:script-update|Install/update native scripts:{exe} -noop --preexec `{spec1} {glroot} "{arg1}" "{arg2}"`
 #
 ## Requires: - glutil-2.5 or above
 ##             curl
@@ -36,7 +36,7 @@
 #
 BASE_PATH=`dirname "${0}"`
 
-GLROOT="${2}"
+GLROOT="${1}"
 
 script_get_version()
 {
@@ -105,18 +105,15 @@ script_process_source()
 			exit 2
 		}
 		
-		[ -n "${add_filt}" ] && {
-			echo "${name}" | egrep -q "^${add_filt}$" && {
-				continue
-			}
+		echo "${name}" | egrep -q "^${2}$" || {
+			[ ${VERBOSE} -gt 1 ] && echo "NOTICE: ${name}: ${path}: filter caught item, skipping.."
+			continue
 		}
-		
-		add_filt="${add_filt}|${name}"
-		
+				
 		path=`echo "${item}" | cut -d' ' -f2`
 		
 		[ -z "${path}" ] && {
-			echo "ERROR: bad configuration, missing script path: ${name}"
+			echo "ERROR: ${name}: bad configuration, missing script path: ${name}"
 			exit 2
 		}
 		
@@ -125,7 +122,7 @@ script_process_source()
 		if [[ ${opt} -eq 2 ]]; then
 			[ -d "${GLROOT}/${BASE_SEARCHDIR}" ] &&
 				touch "${GLROOT}/${BASE_SEARCHDIR}/${path}"
-			[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${path}: updates disabled"
+			[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${name}: ${path}: updates disabled"
 			continue
 		elif [[ ${opt} -eq 4 ]]; then
 			[ -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" ] && 
@@ -134,95 +131,121 @@ script_process_source()
 			continue
 		elif [[ ${opt} -eq 1 ]]; then
 			[ -f "${GLROOT}${BASE_SEARCHDIR}/${path}" ] && {
-				[ ${VERBOSE} -gt 0 ] &&  echo "WARNING: ${path}: configuration file already exists"
+				[ ${VERBOSE} -gt 0 ] &&  echo "WARNING: ${name}: ${path}: configuration file already exists"
 				continue
 			}
 		fi
 		
-		echo "${name}" | egrep -q "^${2}$" && {
-			${CURL} ${CURL_FLAGS} "${BASE_URL}/${path}" > /tmp/glutil.script_update.$$.tmp || {
-				echo "ERROR: ${CURL}: could not fetch: '${BASE_URL}/${path}'"
-				spc_ret=2
+		echo "${3}" | egrep -q '1' && {
+			[ -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" ] || {
+				[ ${VERBOSE} -gt 0 ] && echo "NOTICE: ${name}: ${path}: doesn't exist, skipping.."
 				continue
 			}
-			
-			[ -f /tmp/glutil.script_update.$$.tmp ] || {
-				echo "ERROR: ${path}: missing temp file ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
-				spc_ret=2
-				continue
-			}
-			
-			b_size=`stat -c %s /tmp/glutil.script_update.$$.tmp`
-			
-			[ ${b_size} -eq 0 ] && {
-				echo "ERROR: ${path}: recieved no data ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
-				spc_ret=2
-				continue
-			}
-			
-			[ ${b_size} -lt 50 ] && {
-				echo "ERROR: ${path}: recieved invalid data ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
-				spc_ret=2
-				continue
-			}
-			
-			TARGET_DIR=`dirname "${GLROOT}/${BASE_SEARCHDIR}/${path}"`
-			[ -d "${TARGET_DIR}" ] || {
-				mkdir -p "${TARGET_DIR}" || {
-					echo "ERROR: could not create '${GLROOT}/${BASE_SEARCHDIR}/${path}'"
-					spc_ret=$[spc_ret+1]
-					continue
-				}
-			}
-			
-			if [ -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" ]; then 
-				mode="UPDATE"
-				current_ver=`script_get_version "${GLROOT}/${BASE_SEARCHDIR}/${path}"``script_get_revision "${GLROOT}/${BASE_SEARCHDIR}/${path}"`
-				upgrade_ver=`script_get_version /tmp/glutil.script_update.$$.tmp``script_get_revision /tmp/glutil.script_update.$$.tmp`
-				
-				if [ ${upgrade_ver} -lt ${current_ver} ]; then
-					echo "WARNING: ${path}: current version is newer than remote (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` > `script_join_verstring /tmp/glutil.script_update.$$.tmp`), not updating.."
-					continue
-				fi
-				
-				if [ ${upgrade_ver} -eq ${current_ver} ]; then
-					[ ${VERBOSE} -gt 0 ] && echo "WARNING: ${path}: already installed (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` = `script_join_verstring /tmp/glutil.script_update.$$.tmp`), not updating.."
-					continue
-				fi
-				
-				echo "${mode}: ${path}: (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` -> `script_join_verstring /tmp/glutil.script_update.$$.tmp`)"
-			else
-				mode="INSTALL"
-				echo "${mode}: ${path}: (`script_join_verstring /tmp/glutil.script_update.$$.tmp`)"
-			fi
-					
-			cat /tmp/glutil.script_update.$$.tmp > "${GLROOT}${BASE_SEARCHDIR}/${path}" || {
-				spc_ret=2
-				echo "ERROR: ${path}: update failed ( /tmp/glutil.script_update.$$.tmp )"
-				continue
-			}
-			
-			perm_mask=`echo "${item}" | cut -d' ' -f3`
+		}		
 		
-			[ -z "${perm_mask}" ] && {		
-				perm_mask=755
-			}
-			
-			chmod ${perm_mask} "${GLROOT}/${BASE_SEARCHDIR}/${path}"
-			
-			opt_a=`echo "${item}" | cut -d' ' -f5`			
-			
-			[[ ${opt_a} -eq 1 ]] && { 
-				INPUT_SOURCES=()
-				. "${GLROOT}/${BASE_SEARCHDIR}/${path}" || {
-					echo "ERROR: ${in_source}: failed loading source"
-					exit 1
-				}
-				
-				[ -n "${INPUT_SOURCES}" ] && [ ${VERBOSE} -gt 0 ] && 
-					echo "NOTICE: ${path}: processing sources" && script_process_source INPUT_SOURCES[@] "${2}"
+		[ -n "${add_filt}" ] && {
+			echo "${name}" | egrep -q "^${add_filt}$" && {
+				[ ${VERBOSE} -gt 2 ] && echo "NOTICE: ${name}: ${path}: already processed"
+				continue
 			}
 		}
+		
+		add_filt="${add_filt}|${name}"
+		
+		${CURL} ${CURL_FLAGS} "${BASE_URL}/${path}" > /tmp/glutil.script_update.$$.tmp || {
+			echo "ERROR: ${name}: ${CURL}: could not fetch: '${BASE_URL}/${path}'"
+			spc_ret=2
+			continue
+		}
+		
+		[ -f /tmp/glutil.script_update.$$.tmp ] || {
+			echo "ERROR: ${name}: ${path}: missing temp file ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
+			spc_ret=2
+			continue
+		}
+		
+		b_size=`stat -c %s /tmp/glutil.script_update.$$.tmp`
+		
+		[ ${b_size} -eq 0 ] && {
+			echo "ERROR: ${name}: ${path}: recieved no data ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
+			spc_ret=2
+			continue
+		}
+		
+		[ ${b_size} -lt 50 ] && {
+			echo "ERROR: ${name}: ${path}: recieved invalid data ( ${BASE_URL}/${path} -> /tmp/glutil.script_update.$$.tmp )"
+			spc_ret=2
+			continue
+		}
+		
+		TARGET_DIR=`dirname "${GLROOT}/${BASE_SEARCHDIR}/${path}"`
+		[ -d "${TARGET_DIR}" ] || {
+			mkdir -p "${TARGET_DIR}" || {
+				echo "ERROR: could not create '${GLROOT}/${BASE_SEARCHDIR}/${path}'"
+				spc_ret=$[spc_ret+1]
+				continue
+			}
+		}
+		
+		if [ -f "${GLROOT}/${BASE_SEARCHDIR}/${path}" ]; then 
+			mode="UPDATE"
+			current_ver=`script_get_version "${GLROOT}/${BASE_SEARCHDIR}/${path}"``script_get_revision "${GLROOT}/${BASE_SEARCHDIR}/${path}"`
+			upgrade_ver=`script_get_version /tmp/glutil.script_update.$$.tmp``script_get_revision /tmp/glutil.script_update.$$.tmp`
+			
+			if [ ${upgrade_ver} -lt ${current_ver} ]; then
+				echo "WARNING: ${name}: ${path}: current version is newer than remote (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` > `script_join_verstring /tmp/glutil.script_update.$$.tmp`), not updating.."
+				continue
+			fi
+			
+			if [ ${upgrade_ver} -eq ${current_ver} ]; then
+				[ ${VERBOSE} -gt 0 ] && echo "WARNING: ${name}: ${path}: already installed (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` = `script_join_verstring /tmp/glutil.script_update.$$.tmp`), not updating.."
+				continue
+			fi
+			
+			echo "${mode}: ${path}: (`script_join_verstring "${GLROOT}/${BASE_SEARCHDIR}/${path}"` -> `script_join_verstring /tmp/glutil.script_update.$$.tmp`)"
+		else
+			mode="INSTALL"
+			echo "${mode}: ${path}: (`script_join_verstring /tmp/glutil.script_update.$$.tmp`)"
+		fi
+				
+		cat /tmp/glutil.script_update.$$.tmp > "${GLROOT}${BASE_SEARCHDIR}/${path}" || {
+			spc_ret=2
+			echo "ERROR: ${path}: update failed ( /tmp/glutil.script_update.$$.tmp )"
+			continue
+		}
+		
+		perm_mask=`echo "${item}" | cut -d' ' -f3`
+	
+		[ -z "${perm_mask}" ] && {		
+			perm_mask=755
+		}
+		
+		chmod ${perm_mask} "${GLROOT}/${BASE_SEARCHDIR}/${path}"
+		
+		opt_a=`echo "${item}" | cut -d' ' -f5`			
+		
+		[[ ${opt_a} -eq 1 ]] && { 
+			INPUT_SOURCES=()
+			. "${GLROOT}/${BASE_SEARCHDIR}/${path}" || {
+				echo "ERROR: ${in_source}: failed loading source"
+				exit 1
+			}
+			
+			[ -n "${INPUT_SOURCES}" ] && [ ${VERBOSE} -gt 0 ] && {
+				echo "NOTICE: ${name}: ${path}: processing sources"
+				
+				p_flags="${3}"
+				
+				echo "${3}" | egrep -q '3' && {
+					echo "${3}" | egrep -q '1' || {
+						p_flags="${p_flags}1"
+					}
+				}
+				
+				script_process_source INPUT_SOURCES[@] "${2}" ${p_flags}
+			}
+		}
+		
 	done
 	
 	add_filt=`echo "${add_filt}" | sed -r 's/(\|$)|(^\|)//g'`
@@ -238,10 +261,11 @@ INIT_SOURCES=(
 
 trap "rm -f /tmp/glutil.script_update.$$.tmp" EXIT
 
+
 add_filt=""
 
 if [ -d "${BASE_PATH}/script_update.d" ]; then
-	script_process_source INIT_SOURCES[@] ".*"
+	script_process_source INIT_SOURCES[@] ".*" 3
 else
 	script_process_source INIT_SOURCES[@] ".*" || {
 		echo "ERROR: could not initialize '${BASE_PATH}/script_update.d'"
@@ -251,13 +275,18 @@ else
 	mkdir -p "${BASE_PATH}/script_update.d"
 fi
 
-if [ "${1}" = all ]; then
-	match=".*"
+flags=""
+
+if [ "${2}" = "update" ]; then	
+	flags="${flags}1"
+elif [ "${2}" = "install" ]; then
+	flags="${flags}0"
 else
-	match="${1}"
+	echo "ERROR: invalid argument: '${2}'"
+	exit 1
 fi
 
-
+[ -n "${3}" ] && match="${3}" || match=".*"
 
 for in_source in "${BASE_PATH}/script_update.d"/*; do
 	INPUT_SOURCES=()
@@ -270,6 +299,6 @@ done
 
 [ ${VERBOSE} -gt 0 ] && echo "NOTICE: processing main sources"
 
-script_process_source SOURCES[@] "${match}"
+script_process_source SOURCES[@] "${match}" ${flags}
 
 exit 0
