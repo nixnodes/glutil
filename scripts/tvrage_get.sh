84c2bdb9dbf4c5bbeedbc9ef7471374c3540025f
nixnodes <code@nixnodes.net>
2015-01-08 20:49:52 +0100

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/tvrage_get.sh
+++ scripts/tvrage_get.sh
@@ -17,7 +17,7 @@
 #
 # DO NOT EDIT/REMOVE THESE LINES
 #@VERSION:3
-#@REVISION:44
+#@REVISION:45
 #@MACRO:tvrage|TVRage lookups based on folder names (filesystem) [-arg1=<path>] [-arg2=<path regex>]:{exe} -x {arg1} -lom "depth>0" --silent --dir --sort asc,mtime --preexec "{exe} --tvlog={q:tvrage@file} --backup tvrage" -execv `{spec1} \{basepath\} \{exe\} \{tvragefile\} \{glroot\} \{siterootn\} \{path\} 0 0 '' 3` {arg2}
 #@MACRO:tvrage-d|TVRage lookups based on folder names (dirlog) [-arg1=<regex filter>]:{exe} -d --silent --loglevel=1 --preexec "{exe} --tvlog={q:tvrage@file} --backup tvrage" -execv `{spec1} \{basedir\} \{exe\} \{tvragefile\} \{glroot\} \{siterootn\} \{dir\} 0 0 '' {arg3}` -l: dir -regexi "{arg1}" {arg2} 
 #@MACRO:tvrage-su|Update existing tvlog records, pass query/dir name through the search engine:{exe} -h --tvlog={q:tvrage@file} --silent --loglevel=1 --preexec "{exe} --tvlog={q:tvrage@file} --backup tvrage" -execv `{spec1} \{basedir\} \{exe\} \{tvragefile\} \{glroot\} \{siterootn\} \{dir\} 1`
@@ -122,7 +122,7 @@ TITLE_WIPE_CHARS="\'\´\:\""
 ## in the respective order to show name before it
 ## gets written
 #
-TITLE_REPLACE=("s/ \& / and /")
+TITLE_REPLACE=("s/\&amp\;/and/g" "s/ \& / and /g")
 #
 ############################[ END OPTIONS ]##############################
 ############################[ BEGIN PATHS ]##############################
