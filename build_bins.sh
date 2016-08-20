ff1fa2d747845ebe2ec2212379664a10200054e2
nixnodes <code@nixnodes.net>
2016-08-20 19:35:26 +0200

    nope nope nope

--- build_bins.sh
+++ build_bins.sh
@@ -11,9 +11,6 @@ build()
 	make clean || return 2
 	./configure ${1} || return 2
 	make -j2 LDFLAGS="${LDFLAGS} ${2}" || return 2
-	for i in ${3}; do
-		cp src/${i} ${OUT}/${i}${4} || return 2
-	done
 	[ "${INSTALL}" = "true" ] && {
 		make install || return 2	
 	}
@@ -22,9 +19,9 @@ build()
 
 rm -f "${OUT}"/*
 
-build "--enable-precheck ${1}" "${3}" "glutil-precheck" || exit 2
-build "${1} ${2}" "${3}" "glutil" || exit 2
-build "--enable-gfind ${1}" "${3}" "gfind" || exit 2
-build "--enable-chroot-ownbin ${1} ${2}" "${3}" "glutil-chroot" || exit 2
+build "--enable-precheck ${1}" "${3}" || exit 2
+build "${1} ${2}" "${3}" || exit 2
+build "--enable-gfind ${1}" "${3}" || exit 2
+build "--enable-chroot-ownbin ${1} ${2}" "${3}" || exit 2
 
 exit 0
